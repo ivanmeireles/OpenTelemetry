8640056b6c943fcb6b519dc7c92905f4ad896717
Ivan Meireles <ivanmds2@hotmail.com>
2025-01-08 15:32:04 -0300

    add config tracing to massTransit

---
+++ Otel.MassTransit/Extensions/MassTransitLoadTrancingExtension.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit.Logging;
+using OpenTelemetry.Trace;
+
+namespace Otel.MassTransit.Extensions
+{
+    public static class MassTransitLoadTrancingExtension
+    {
+        public static TracerProviderBuilder ConfigureMassTransitTracing(this TracerProviderBuilder builder)
+        {
+            builder.AddSource(DiagnosticHeaders.DefaultListenerName);
+            return builder;
+        }
+    }
+}
