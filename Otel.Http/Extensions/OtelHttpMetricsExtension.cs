466f23f7695247cb544c7780a9715ccdd45a81fd
Ivan Meireles Da Silva <ivanmds2@hotmail.comn>
2025-05-21 17:25:55 -0300

    add project http

---
+++ Otel.Http/Extensions/OtelHttpMetricsExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Otel.Sdk.Exceptions;
+using Otel.Sdk.HttpMiddleware;
+using Otel.Sdk.Metric;
+
+namespace Otel.Http.Extensions
+{
+    internal static class OtelHttpMetricsExtension
+    {
+        public static IApplicationBuilder UseMetrics(this IApplicationBuilder builder)
+        {
+            var metricService = builder.ApplicationServices.GetService<IMetricService>();
+            var meter = metricService?.GetCurrentMeter();
+
+            if (meter is null)
+                throw new MetricStartupException();
+
+            HttpRequestMetricsMiddleware.HTTP_REQUEST_ELAPSED_TIME = meter.CreateHistogram<int>("http_request_elapsed_time");
+
+            return builder.UseMiddleware<HttpRequestMetricsMiddleware>();
+        }
+    }
+}
---
+++ Otel.Http/HttpMiddleware/HttpRequestMetricsMiddleware.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Otel.Sdk.Extensions;
+using Otel.Sdk.Metric;
+
+
+namespace Otel.Sdk.HttpMiddleware
+{
+    internal class HttpRequestMetricsMiddleware
+    {
+        internal static Histogram<int> HTTP_REQUEST_ELAPSED_TIME;
+        private readonly RequestDelegate _next;
+        private readonly ILogger<HttpRequestMetricsMiddleware> _logger;
+
+        public HttpRequestMetricsMiddleware(RequestDelegate next, ILogger<HttpRequestMetricsMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var path = context.Request.Path.Value.ToLower();
+            if (OpenTelemetryMetricsExtension.OTLP_CONFIG?.MetricConfig?.IgnorePathStartWith?.Count > 0)
+            {
+                var shouldIgnoreMetrics = false;
+                foreach (var ignorePathStartWith in OpenTelemetryMetricsExtension.OTLP_CONFIG.MetricConfig.IgnorePathStartWith)
+                {
+                    shouldIgnoreMetrics = path.StartsWith(ignorePathStartWith);
+                    if (shouldIgnoreMetrics)
+                        break;
+                }
+
+                if (shouldIgnoreMetrics)
+                {
+                    await _next(context);
+                    return;
+                }
+            }
+
+            var activity = Activity.Current;
+            Stopwatch watch = Stopwatch.StartNew();
+            List<KeyValuePair<string, object?>> listTags = new List<KeyValuePair<string, object>>();
+
+            listTags.Add(MetricService.CreateTag("http_method", context.Request.Method));
+            listTags.Add(MetricService.CreateTag("route", GetRoute(context)));
+
+            if (activity != null)
+            {
+                activity.SetTag(ConstValues.KEY_CORRELATION_ID, GetCorrelationId(context));
+                string value = context.Connection.RemoteIpAddress.ToString();
+                activity.SetTag("http_remote_ip_address", value);
+            }
+
+            try
+            {
+                await _next(context);
+                int statusCode = context.Response.StatusCode;
+                listTags.Add(MetricService.CreateTag("http_status_code", statusCode));
+                if (statusCode >= 200 && statusCode <= 299)
+                {
+                    listTags.Add(MetricService.CreateTag("http_status_code_family", "200"));
+                }
+                else if (statusCode >= 400 && statusCode <= 499)
+                {
+                    listTags.Add(MetricService.CreateTag("http_status_code_family", "400"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Exception ex2 = ex.InnerException ?? ex;
+                listTags.Add(MetricService.CreateTag("http_status_code_family", "500"));
+                listTags.Add(MetricService.CreateTag("exception_full_name", ex2.GetType().FullName));
+                _logger.LogError(ex.Message, ex);
+                throw ex;
+            }
+            finally
+            {
+                watch.Stop();
+                int value2 = (int)watch.ElapsedMilliseconds;
+                HTTP_REQUEST_ELAPSED_TIME.Record(value2, listTags.ToArray());
+            }
+        }
+        private string GetCorrelationId(HttpContext context)
+        {
+            StringValues correlationIdValue = default;
+            context.Request.Headers.TryGetValue(ConstValues.KEY_CORRELATION_ID, out correlationIdValue);
+
+            var correlationId = correlationIdValue.ToString();
+            if (string.IsNullOrEmpty(correlationId))
+                correlationId = "none";
+
+            return correlationId;
+        }
+
+        private string GetRoute(HttpContext context)
+        {
+            var endpoint = context.GetEndpoint();
+            return endpoint?.Metadata
+                .GetMetadata<Microsoft.AspNetCore.Routing.RouteEndpoint>()?.RoutePattern?.RawText ?? "";
+        }
+    }
+}
